34146405d6a3bb57d27c1ba56bdba65a4ba80ad7
Dagan Sandler <dagansandler@gmail.com>
2018-01-11 16:49:24 +0200

    added MyController, instantiate it and call hello() method

---
+++ src/main/java/io/github/dagansandler/didemo/controllers/MyController.java
@@ -0,0 +1,12 @@
+package io.github.dagansandler.didemo.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public void hello() {
+        System.out.println("hello");
+    }
+
+}
