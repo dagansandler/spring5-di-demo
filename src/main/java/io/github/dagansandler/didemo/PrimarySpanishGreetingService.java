bd35dc3d5031c4faf56503227e782c24545073f1
Dagan Sandler <dagansandler@gmail.com>
2018-02-21 17:51:48 +0200

    added primary bean for es profile

--- src/main/java/io/github/dagansandler/didemo/PrimarySpanishGreetingService.java
+++ src/main/java/io/github/dagansandler/didemo/PrimarySpanishGreetingService.java
@@ -1,4 +1,4 @@
-package io.github.dagansandler.didemo.controllers;
+package io.github.dagansandler.didemo;
 
 import io.github.dagansandler.didemo.services.GreetingService;
 import org.springframework.context.annotation.Primary;
@@ -6,13 +6,13 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
-@Profile("en")
+@Profile("es")
 @Primary
-public class PrimaryGreetingService implements GreetingService {
+public class PrimarySpanishGreetingService implements GreetingService {
 
     @Override
     public String sayGreeting() {
-        return "Hello - Primary Greeting Service";
+        return "Nobody expects the Spanish Greeting Service";
     }
 
 }
